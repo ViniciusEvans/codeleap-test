3a89080ae4d245c6c69e58bb0e9dda2ac6246b3b
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-26 00:40:14 -0300

    deleted unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.scss";
-import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import RoutesController from "./routes";
