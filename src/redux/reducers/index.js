956fa4e2ba817a77050f6f3cfb66f092c715cc14
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-25 22:45:50 -0300

    create toggle state button reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,21 @@
-export default function reducer(state = "a", action) {
-  return state;
+const initialState = {
+  stateButton: true,
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "ENABLE_BUTTON": {
+      const newState = { ...state };
+      newState.stateButton = false;
+      return newState;
+    }
+    case "DISABLE_BUTTON": {
+      const newState = { ...state };
+      newState.stateButton = true;
+      return newState;
+    }
+
+    default:
+      return state;
+  }
 }
