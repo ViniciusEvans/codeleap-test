46efab7824c36ec2d761bf628837e235dfa0c0d9
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-25 22:43:55 -0300

    centralizing routes

---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/signup";
+import Home from "./pages/home";
+import RequiredUsername from "./components/requiredUsername";
+
+function RoutesController() {
+  return (
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={<RequiredUsername />}>
+        <Route path="home" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default RoutesController;
