b8129eeb74d644d1d707aa73156324dd04c4d9e1
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-28 19:19:35 -0300

    setting home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,19 @@
+import "./style.scss";
+import CreatePost from "../../components/createPost";
+
 function Home() {
   return (
     <div className="home">
-      <h1>home</h1>
+      <div className="home-body-app">
+        <div className="home-app-container">
+          <div className="home-container-header">
+            <h1 className="home--header">CodeLeap Network</h1>
+          </div>
+          <div className="app-container-post">
+            <CreatePost />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
