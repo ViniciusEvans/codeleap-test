a9acf3d0f5f613111786b4aeae105f73bd3d459b
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-29 17:35:49 -0300

    changed how to get props

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -1,28 +1,37 @@
 import "./style.scss";
 import editIcon from "../../assets/edit-icon.svg";
 import deleteIcon from "../../assets/delete-icon.svg";
+import calcTime from "../../services/getTIme";
 
-function PostComponent({ title, username, time, content }) {
+function PostComponent(props) {
   return (
-    <div className="post-container">
+    <div className="post-container" key={props.id}>
       <div className="post-header">
-        <h1 className="post-title">{title}</h1>
-        <div className="delete-edit-button">
-          <button className="btn delete-btn">
-            <img src={deleteIcon} alt="delete icon" />
-          </button>
-          <button className="btn edit-btn">
-            <img src={editIcon} alt="delete icon" />
-          </button>
-        </div>
+        <h1 className="post-title">{props.title}</h1>
+        {props.username === props.loggedUser && (
+          <div className="delete-edit-button">
+            <button
+              onClick={() => console.log(props.id)}
+              className="btn delete-btn"
+            >
+              <img src={deleteIcon} alt="delete icon" />
+            </button>
+            <button
+              onClick={() => console.log(props.id)}
+              className="btn edit-btn"
+            >
+              <img src={editIcon} alt="delete icon" />
+            </button>
+          </div>
+        )}
       </div>
       <div className="post--content--section">
         <div className="post-infos">
-          <span className="post--username">@{username}</span>
-          <span className="post--time">{time}</span>
+          <span className="post--username">@{props.username}</span>
+          <span className="post--time">{calcTime(props.time) + " ago"}</span>
         </div>
         <div className="post-content">
-          <p>{content}</p>
+          <p>{props.content}</p>
         </div>
       </div>
     </div>
