60c901f461c1e3868e70e03319b8aa421bad1604
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-29 16:59:46 -0300

    added persisted username  provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.scss";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 import RoutesController from "./routes";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <RoutesController />
+        <PersistGate persistor={persistor}>
+          <RoutesController />
+        </PersistGate>
       </Provider>
     </div>
   );
