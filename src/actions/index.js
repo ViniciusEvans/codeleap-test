fee7017c66f6b5a461d0f19b02a949d35254d5a6
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-29 22:50:16 -0300

    added edit, get and delete actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,27 @@ export function handleEnableButton() {
 export function handleDisableButton() {
   return { type: types.DISABLE_BUTTON };
 }
+export function disablePostButton() {
+  return { type: types.DISABLE_POST };
+}
+export function enablePostButton() {
+  return { type: types.ENABLE_POST };
+}
 export function handleSubmitSignup(inputSignupValue) {
   return { type: types.SUBMIT_SIGNUP, payload: { username: inputSignupValue } };
+}
+export function createPostSuccess() {
+  return { type: types.CREATE_POST_SUCCESS };
+}
+export function createPostFailure(error) {
+  return { type: types.CREATE_POST_FAILURE, error_message: error };
+}
+export function editPost(content = "", title = "", id = "") {
+  return { type: types.EDIT_POST, payload: { content, title, id } };
+}
+export function deletePost(id = "") {
+  return { type: types.DELETE_POST, id };
+}
+export function getPost() {
+  return { type: types.GET_POST };
 }
