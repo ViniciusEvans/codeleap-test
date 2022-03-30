eed891dee44bd55887bcd848585f52505dc30694
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-29 22:51:07 -0300

    added fetch to delete post

--- src/components/confirmDeleteModal/index.jsx
+++ src/components/confirmDeleteModal/index.jsx
@@ -1,6 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./style.scss";
+import * as actions from "../../actions/index";
 
 function ConfirmDeleteModal() {
+  const reducerStates = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  async function deletePost() {
+    try {
+      await fetch(
+        "https://dev.codeleap.co.uk/careers/" +
+          reducerStates.reducer.deletePostSetup.id +
+          "/",
+        {
+          method: "DELETE",
+          headers: {
+            "content-type": "application/json",
+          },
+        }
+      );
+    } catch (error) {
+      dispatch(actions.createPostFailure(error.message));
+    } finally {
+      dispatch(actions.getPost());
+      dispatch(actions.deletePost());
+    }
+  }
   return (
     <div className="delete-modal">
       <div className="delete-modal-container">
@@ -8,8 +33,15 @@ function ConfirmDeleteModal() {
           <p>Are you sure you want to delete this item?</p>
         </div>
         <div className="delete-modal-buttons">
-          <button className="btn btn-cancel">Cancel</button>
-          <button className="btn btn-confirm">Confirm</button>
+          <button
+            className="btn btn-cancel"
+            onClick={() => dispatch(actions.deletePost())}
+          >
+            Cancel
+          </button>
+          <button className="btn btn-confirm" onClick={deletePost}>
+            Confirm
+          </button>
         </div>
       </div>
     </div>
